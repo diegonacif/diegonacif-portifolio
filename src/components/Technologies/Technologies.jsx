c4befa909fd7ba0680e6d4ca3cb04cf3a12c8cff
diegonacif <diegoreisnacif@gmail.com>
2022-11-15 22:36:10 -0300

    feat: technologies component

--- src/components/Technologies/Technologies.jsx
+++ src/components/Technologies/Technologies.jsx
@@ -1,9 +1,31 @@
 import '../../styles/App.css';
 
+import htmlImg from '../../assets/html5.png';
+import cssImg from '../../assets/css3.png';
+import jsImg from '../../assets/js.png';
+
 export const Technologies = () => {
   return (
     <div className="technologies-container">
-      <h1>Technologies</h1>
+      <header className="tech-title">
+        <span>&lt;</span>
+        <h2>Tecnologias</h2>
+        <span>&gt;</span>
+      </header>
+      <section className="techs-wrapper">
+        <div className="tech-card">
+          <img src={htmlImg} alt="html5 logo" />
+          <span>HTML5</span>
+        </div>
+        <div className="tech-card">
+          <img src={cssImg} alt="html5 logo" />
+          <span>CSS3</span>
+        </div>
+        <div className="tech-card">
+          <img src={jsImg} alt="html5 logo" id="js-img" />
+          <span>JavaScript</span>
+        </div>
+      </section>
     </div>
   );
 };
